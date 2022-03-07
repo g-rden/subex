afe0f09b6dd0b00df092f657af68c1a31bc754df
g-rden <94605617+g-rden@users.noreply.github.com>
2022-03-07 18:27:38 +0100

    simplified a lot

--- subex.sh
+++ subex.sh
@@ -1,32 +1,17 @@
 #!/bin/sh
-
+#
 # goes to all visible subdirectories sequentially and executes a command there
-# dependencies: pure POSIX Shell
-
-exec 2>/dev/null # supress errors
+# dependencies: POSIX Shell
+#
 
 # show help
-! (: "${*:?}" ) && echo 'Usage: subex [command_string ...]' && exit
+! (: "${*:?}") 2>/dev/null && echo 'Usage: subex [command_string ...]' && exit
 
-dirs="$(printf '%s' */)" # list of every visible subdir
-startdir="$PWD"          # remember where subex was executed
-cmd="$*"                 # passed commands
+startdir="$PWD" # remember where subex was executed
 
-# main function
-do_stuff() {
-	curdir="${dirs%%/*}" # first entry of dirs
-	dirs="${dirs#*/}"    # removes first entry of dirs
-	
-	cd "$curdir" || { echo "$PWD: can't go to curdir: '$curdir'"; exit; }
+for curdir in $(echo */); do
+	cd "$curdir" || { echo "$PWD: can't go to curdir: $curdir"; exit;}
 	echo "$PWD"
-	eval "$cmd"
-	cd "$startdir" || { echo "$PWD: can't go to startdir: '$startdir'"; exit; }
-}
-
-# main loop
-while :; do
-	case "$dirs" in
-		*/*/) do_stuff;;
-		*/) do_stuff; break;; # for the last dir
-	esac
+	eval "$*"
+	cd "$startdir" || { echo "$PWD: can't go to startdir: $startdir"; exit;}
 done
